1cdc3f0b1465d110783c047c0f86781f3e7365b7
leandro prates <leandro.prates@gmail.com>
2021-03-02 08:34:52 -0300

    config cliente

---
+++ src/main/java/com/example/configcliente/ConfigClienteApplication.java
@@ -0,0 +1,13 @@
+package com.example.configcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/configcliente/ConfigClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
